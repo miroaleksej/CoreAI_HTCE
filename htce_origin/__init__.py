f02051ee0ba35f661b879f47bf64d0e0915a5d2c
RusQLab <99619093+miroaleksej@users.noreply.github.com>
2026-06-14 23:12:44 +0300

    Update __init__.py

--- htce_origin/__init__.py
+++ htce_origin/__init__.py
@@ -1,4 +1,8 @@
-__version__ = "1.0.0-final_math-q256-clean"
+"""HTCE-Origin clean v1.0 Q256 runtime."""
+
+__version__ = "1.0.0+final.math.q256.clean"
+__release_line__ = "v1.0-final_math-q256-clean"
+__modulus_family__ = "Q256"
 
 from htce_origin.body.runtime import HTCERuntime, RuntimeConfig, RuntimeRequest, RuntimeResponse
 
